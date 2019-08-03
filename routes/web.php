1e4ad7dee15c23bc23d064dd1ea37f34550f2250
ibrahim.shareef1988@outlook.com <ibrahim.shareef1988@outlook.com>
2019-08-03 21:09:19 +0500

    a

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,13 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/a', function () {
+    return view('welcome');
+});
